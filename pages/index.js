748509f44d1305e6fc6e5902d81bd2ec0f34e618
edu-bit <52946883+edu-bit@users.noreply.github.com>
2026-04-21 22:59:36 +0000

    add services scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Este é o meu primeiro site público!!!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação erradaaa)");
-}
-
 export default Home;
